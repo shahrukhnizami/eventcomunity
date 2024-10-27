232de6dc5be9828cc8b0ac9320000bc7b99ca960
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-10-27 08:00:44 +0500

    Revert "Category in table"
    This reverts commit 679bbca691e096b02059190743e0a08807005a60.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      remotePatterns: [
-        { hostname: "images.unsplash.com" }
-      ],
+      remotePatterns: [{ hostname: "images.unsplash.com" }],
     },
-   
   };
   
   export default nextConfig;
