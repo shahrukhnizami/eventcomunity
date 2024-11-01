7a03aee02492b21d96cdca241529018168dd5881
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-11-02 03:21:57 +0500

    google signin

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,48 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { auth, signIn, signOut } from "../../auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
   return (
 
 
 <div className="min-h-screen ">
     <div className="flex justify-around items-center my-4">
         <h1 className="font-bold text-3xl text-center">Wellcome Event Comunity</h1>
-        
+        {
+          session?
+          <div className="flex gap-5">
+            <Image src={session.user?.image}
+                height={30}
+                width={30}
+                className="rounded-full" />
+            <h1 className="font-bold">{session?.user?.email}
+          </h1>
+          <form
+          action={async () => {
+            "use server"
+            await signOut("google")
+          }}
+        >
+          <button type="submit">Sign Out</button>
+        </form>
+          </div>:
+          <div className="text-right py-2">
+           <form
+                action={async () => {
+                  "use server"
+                  await signIn("google")
+                }}
+              >
+                <button type="submit">Signin with Google</button>
+              </form>
+          </div>
+         
+        }
       </div>
-      <div className="text-center py-2">
-      <Link href="admin/dashboard"><Button variant="outline">Admin Dashboard</Button></Link>
-      </div>
+      
       </div>
     // <div className="min-h-screen ">
     //  <h1 className="text-3xl font-bold text-center p-20">Event Home</h1>
---
+++ src/app/signin/page.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { auth, signIn, signOut } from "../../../auth"
+import { redirect } from "next/dist/server/api-utils";
+import Admin from "../(admin)/admin/page";
+
+export default async function Signin(){
+const session = await auth();
+console.log("session",session);
+
+    return (
+    <div className="flex items-center container mx-auto justify-center min-h-screen">
+        {
+            session?
+            <div className="flex flex-col rounded">
+                
+                <Image src={session.user?.image}
+                height={100}
+                width={100}
+                className="rounded-full" />
+                <h1 className="font-bold py-3">{session?.user?.name}</h1>
+                <h1 className="py-2">{session.user?.email}</h1>
+                <form
+                action={async () => {
+                  "use server"
+                  await signOut()
+                }}
+              >
+                <button type="submit">Sign Out</button>
+              </form>
+                </div>:
+                <form
+                action={async () => {
+                  "use server"
+                  await signIn("google")
+                }}
+              >
+                <button type="submit">Signin with Google</button>
+              </form>
+        }
+        
+    </div>
+        )
+    
+}
