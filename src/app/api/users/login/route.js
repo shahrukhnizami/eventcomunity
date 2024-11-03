bdf97f0b9472e92645d3f748fafaed69a9d5b0f4
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-11-04 01:06:33 +0500

    login Comment

--- src/app/api/users/login/route.js
+++ src/app/api/users/login/route.js
@@ -1,32 +1,32 @@
-import { connectDB } from "@/lib/db/connectionDB";
-import { UserModal } from "@/lib/models/User";
-import jwt from "jsonwebtoken";
+// import { connectDB } from "@/lib/db/connectionDB";
+// import { UserModal } from "@/lib/models/User";
+// import jwt from "jsonwebtoken";
 
 
-export async function POST(request) {
-    await connectDB()
-    const obj = await request.json();
-    const user = await UserModal.findOne({ email: obj.email });
-    if (!user)
-        return Response.json(
-          { error: true, msg: "User not found" },
-          { status: 403 }
-        );
+// export async function POST(request) {
+//     await connectDB()
+//     const obj = await request.json();
+//     const user = await UserModal.findOne({ email: obj.email });
+//     if (!user)
+//         return Response.json(
+//           { error: true, msg: "User not found" },
+//           { status: 403 }
+//         );
     
-      const isPasswordValid = await bcrypt.compare(obj.password, user.password);
+//       const isPasswordValid = await bcrypt.compare(obj.password, user.password);
     
-      if (!isPasswordValid)
-        return Response.json(
-          { error: true, msg: "Password is not valid." },
-          { status: 403 }
-        );
-    var token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_KEY);
-  return Response.json(
-    {
-      msg: "Users Login Successfully ",
-      user,
-      token,
-    },
-    { status: 200 }
-  );
-}
+//       if (!isPasswordValid)
+//         return Response.json(
+//           { error: true, msg: "Password is not valid." },
+//           { status: 403 }
+//         );
+//     var token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_KEY);
+//   return Response.json(
+//     {
+//       msg: "Users Login Successfully ",
+//       user,
+//       token,
+//     },
+//     { status: 200 }
+//   );
+// }
